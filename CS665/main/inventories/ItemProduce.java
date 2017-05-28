94235876838b7d9e47a2e7796737a5c7c72ee44e
Ant Cassetta <Antcassetta@icloud.com>
2017-05-28 13:19:43 -0400

    updating names of Item and Item subclasses for better organization within package

---
+++ CS665/main/inventories/ItemProduce.java
@@ -0,0 +1,9 @@
+package inventories;
+
+public class ItemProduce extends Item{
+
+	public ItemProduce(String givenItemName, int givenItemQuantity, String givenItemType) {
+		super(givenItemName, givenItemQuantity, givenItemType);
+	}
+
+}//end ProduceItem
--- CS665/src/inventories/ProduceItem.java
+++
@@ -1,9 +0,0 @@
-package inventories;
-
-public class ProduceItem extends Item{
-
-	public ProduceItem(String givenItemName, int givenItemQuantity, String givenItemType) {
-		super(givenItemName, givenItemQuantity, givenItemType);
-	}
-
-}//end ProduceItem
