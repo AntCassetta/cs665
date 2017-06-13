2f976214900d510b0195ecdfaa739e5c53318d1e
Ant Cassetta <Antcassetta@icloud.com>
2017-06-13 11:26:08 -0400

    adding a remove inventory Item test and updating documentation

--- CS665/test/raiderInventories/AllTests.java
+++ CS665/test/raiderInventories/AllTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({AddInventoryTest.class, AddItemTest.class, decrementItemNegativeTest.class, decrementItemTest.class,
-		IncrementItemTest.class, updateItemNegativeTest.class, UpdateItemTest.class, clearInventoryManagerRoster.class })
+		IncrementItemTest.class, updateItemNegativeTest.class, UpdateItemTest.class, RemoveItemTest.class,clearInventoryManagerRoster.class })
 public class AllTests {
 
 }
--- CS665/test/raiderInventories/RemoveItemTest.java
+++ CS665/test/raiderInventories/RemoveItemTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import raiderInventories.InventoryManager;
 
-public class AddItemTest {
+public class RemoveItemTest {
 
 	@Test
 	public void test() {
@@ -15,20 +15,21 @@ public class AddItemTest {
 		
 		RaiderInventory testFridge = testManager.getInventory("testFridge", 1);
 		
-		//System.out.println(testFridge.getInventoryName());
 		
 		testManager.addItem(testFridge, "Milk", 2, "Dairy");
 		
 		
-		//assert statements
-		
+		//assert Item was properly added.
 		assertEquals(1, testFridge.itemInventory.size());
 		assertEquals("Milk", testFridge.itemInventory.first().getElement().getItemName());
 		assertEquals(2, testFridge.itemInventory.first().getElement().getItemQuantity());
 		
+		//Perform removal
+		testManager.removeItem(testFridge, "Milk");
+		assertEquals(0, testFridge.itemInventory.size());
+		
 		//clear inventoryContents
 		while (testFridge.itemInventory.size() > 0 ) {testFridge.itemInventory.remove(testFridge.itemInventory.first());}
-	
 	}
 
 }
